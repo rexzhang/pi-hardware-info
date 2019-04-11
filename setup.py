7863335cf6bed3f95fc6226df1a4b6ef7e40a89b
Rex Zhang <rex.zhang@gmail.com>
2019-04-11 17:25:00 +0800

    Update READEM.rst

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
+from setuptools import setup
 
 # To use a consistent encoding
 from codecs import open
@@ -14,7 +14,7 @@ import pi_hardware_info
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
